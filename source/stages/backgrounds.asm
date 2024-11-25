66618e49614ac04a9e57ff99614f7145851f7b47
gustavogx <gustavogx@gmail.com>
2024-11-25 10:41:06 -0300

    folder restructuring

--- source/stages/backgrounds.asm
+++ source/stages/backgrounds.asm
@@ -18,10 +18,10 @@ Data_TitleScreen:
 	.byte PaletteOBJ_TitleScreen - Data_TitleScreen
 	
 	PaletteBG_TitleScreen:
-	.incbin "data/stages/TitleScreen_BPL.bin"
+	.incbin "stages/data/TitleScreen_BPL.bin"
 
 	PaletteOBJ_TitleScreen:
-	.incbin "data/stages/TitleScreen_OPL.bin"
+	.incbin "stages/data/TitleScreen_OPL.bin"
 
 ;; ============== Stage 01 ===============
 Data_Stage01_A:
@@ -29,17 +29,17 @@ Data_Stage01_A:
 	.byte PaletteOBJ_Stage01 - Data_Stage01_A
 	
 	PaletteBG_Stage01_A:
-	.incbin "data/stages/Stage01_A_BPL.bin"
+	.incbin "stages/data/Stage01_A_BPL.bin"
 
 Data_Stage01_B:
 	.addr NameTable_Stage01_B ;$ed8a
 	.byte PaletteOBJ_Stage01 - Data_Stage01_B
 
 	PaletteBG_Stage01_B:
-	.incbin "data/stages/Stage01_B_BPL.bin"
+	.incbin "stages/data/Stage01_B_BPL.bin"
 
 	PaletteOBJ_Stage01:
-	.incbin "data/stages/Stage01_OPL.bin"
+	.incbin "stages/data/Stage01_OPL.bin"
 
 ;; ============== Stage 03 ===============
 Data_Stage03_A: ; $68b8
@@ -47,17 +47,17 @@ Data_Stage03_A: ; $68b8
 	.byte PaletteOBJ_Stage03 - Data_Stage03_A
 	
 	PaletteBG_Stage03_A:
-	.incbin "data/stages/Stage03_A_BPL.bin"
+	.incbin "stages/data/Stage03_A_BPL.bin"
 
 Data_Stage03_B:
 	.addr NameTable_Stage03_B ;$f1f9
 	.byte PaletteOBJ_Stage03 - Data_Stage03_B
 
 	PaletteBG_Stage03_B:
-	.incbin "data/stages/Stage03_B_BPL.bin"
+	.incbin "stages/data/Stage03_B_BPL.bin"
 
 	PaletteOBJ_Stage03:
-	.incbin "data/stages/Stage03_OPL.bin"
+	.incbin "stages/data/Stage03_OPL.bin"
 
 ;; ============== Stage 02 ===============
 Data_Stage02_A:
@@ -65,17 +65,17 @@ Data_Stage02_A:
 	.byte PaletteOBJ_Stage02 - Data_Stage02_A
 	
 	PaletteBG_Stage02_A:
-	.incbin "data/stages/Stage02_A_BPL.bin"
+	.incbin "stages/data/Stage02_A_BPL.bin"
 
 Data_Stage02_B:
 	.addr NameTable_Stage02_B ;$f65a
 	.byte PaletteOBJ_Stage02 - Data_Stage02_B
 	
 	PaletteBG_Stage02_B:
-	.incbin "data/stages/Stage02_B_BPL.bin"
+	.incbin "stages/data/Stage02_B_BPL.bin"
 
 	PaletteOBJ_Stage02:
-	.incbin "data/stages/Stage02_OPL.bin"
+	.incbin "stages/data/Stage02_OPL.bin"
 
 ;; ============== Stage 04 ===============
 Data_Stage04_A:
@@ -83,17 +83,17 @@ Data_Stage04_A:
 	.byte PaletteOBJ_Stage04 - Data_Stage04_A
 
 	PaletteBG_Stage04_A:
-	.incbin "data/stages/Stage04_A_BPL.bin"
+	.incbin "stages/data/Stage04_A_BPL.bin"
 
 Data_Stage04_B:
 	.addr NameTable_Stage04_B ;$fad0
 	.byte PaletteOBJ_Stage04 - Data_Stage04_B
 
 	PaletteBG_Stage04_B:
-	.incbin "data/stages/Stage04_B_BPL.bin"
+	.incbin "stages/data/Stage04_B_BPL.bin"
 
 	PaletteOBJ_Stage04:
-	.incbin "data/stages/Stage04_OPL.bin"
+	.incbin "stages/data/Stage04_OPL.bin"
 
 ;; ============== Ending ===============
 Data_Ending:
@@ -101,20 +101,20 @@ Data_Ending:
 	.byte PaletteOBJ_Ending - Data_Ending
 
 	PaletteBG_Ending:
-	.incbin "data/stages/Ending_BPL.bin"
+	.incbin "stages/data/Ending_BPL.bin"
 
 	PaletteOBJ_Ending:
-	.incbin "data/stages/Ending_OPL.bin"
+	.incbin "stages/data/Ending_OPL.bin"
 
 Data_BlankScreen: 
 	.addr NameTable_Blank ;$e9a0
 	.byte PaletteOBJ_BlankScreen - Data_BlankScreen
 	
 	PaletteBG_BlankScreen:
-	.incbin "data/stages/BlankScreen_BPL.bin"
+	.incbin "stages/data/BlankScreen_BPL.bin"
 
 	PaletteOBJ_BlankScreen:
-	.incbin "data/stages/BlankScreen_OPL.bin"
+	.incbin "stages/data/BlankScreen_OPL.bin"
 
 
 ;; ============ Nametables =============
