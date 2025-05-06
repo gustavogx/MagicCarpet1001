0763e25cbc4560dc4baa681a917cff52b4858e29
gustavogx <gustavogx@gmail.com>
2025-05-06 10:19:38 -0300

    Old compilation system remove

--- build.bat
+++
@@ -1,2 +0,0 @@
-ca65 source\main.asm -o build\main.o --debug-info -I source\ines 
-ld65 build\main.o -o rom\MagicCarpet.nes -C source\config\mapper003.cfg --dbgfile  rom\MMagicCarpet.dbg
--- build.sh
+++
@@ -1,3 +0,0 @@
-ca65 ./source/main.asm -o ./build/main.o --debug-info -I./include
-ld65 ./build/main.o -o ./rom/MagicCarpet.nes -C ./source/config/mapper003.cfg --dbgfile  ./rom/MagicCarpet.dbg
-rm ./build/main.o
